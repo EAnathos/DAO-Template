ae89cc3e6727d2cb9c272a471d09e7d1ef8f1518
Anathos <83123402+EAnathos@users.noreply.github.com>
2023-04-14 13:55:32 +0200

    init commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x78F5A9Ce7471F57F34cC56886aA67F3BF7f4eB33";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
